8bcdeeaf1ef75238df61965356c44be28867dc20
Fred Persyn <frederic.persyn@gmail.com>
2023-01-31 09:19:43 +0000

    separated display resp in BoardUI class

--- 01_single_responsibility/README.md
+++ 01_single_responsibility/README.md
@@ -7,3 +7,6 @@ This example is derived from a Tic Tac Toe game.
 You are provided with a generic Board class that does board-related things — it stores the values of spots on the board, returns the board's rows, and prints the board out to the screen. On the surface, everything seems legitimately related to a real-world Board object, but the *Single Responsibility Principle* tells us that this class is actually handling far too many responsibilities.
 
 How would you correct this situation?
+* Board contains 2 responsibilities: (1) data & domain logic; (2) visualisation;
+* Moving the visualisation responsibility to its own class BoardUI taking a Board as input would resolve SRP.
+* Some other design principle issues remain in the BoardUI class: DRY
---
+++ 01_single_responsibility/src/BoardUI.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardUI {
+    Board board;
+    
+    public BoardUI(Board board) {
+        this.board = board;
+    }
+
+    public List<String> firstRow() {
+        List<String> firstRow = new ArrayList<>();
+        firstRow.add(this.board.spots.get(0));
+        firstRow.add(this.board.spots.get(1));
+        firstRow.add(this.board.spots.get(2));
+        return firstRow;
+    }
+
+    public List<String> secondRow() {
+        List<String> secondRow = new ArrayList<>();
+        secondRow.add(this.board.spots.get(3));
+        secondRow.add(this.board.spots.get(4));
+        secondRow.add(this.board.spots.get(5));
+        return secondRow;
+    }
+
+    public List<String> thirdRow() {
+        List<String> thirdRow = new ArrayList<>();
+        thirdRow.add(this.board.spots.get(6));
+        thirdRow.add(this.board.spots.get(7));
+        thirdRow.add(this.board.spots.get(8));
+        return thirdRow;
+    }
+
+    // Poor code — can you improve this?
+    public void display() {
+        String formattedFirstRow = this.board.spots.get(0) + " | " + this.board.spots.get(1) + " | " + this.board.spots.get(2) + "\n"
+                + this.board.spots.get(3) + " | " + this.board.spots.get(4) + " | " + this.board.spots.get(5) + "\n"
+                + this.board.spots.get(6) + " | " + this.board.spots.get(7) + " | " + this.board.spots.get(8);
+        System.out.print(formattedFirstRow);
+    }
+}
